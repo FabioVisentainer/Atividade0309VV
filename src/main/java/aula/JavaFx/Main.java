14a5b2dce0dbdb2bdcf60e97385e266d53643a84
Nicolas H. Tancon <nicolastancon@gmail.com>
2025-09-03 21:23:15 -0300

    feat: valida se a frase não é vazia ou não contem apenas numeros

--- src/main/java/aula/JavaFx/Main.java
+++ src/main/java/aula/JavaFx/Main.java
@@ -8,6 +8,8 @@ public class Main {
         Frase.encontrarMaiorFrase("O rato roeu a roupa do rei de roma");
         Frase.encontrarMaiorFrase("A jornada de mil milhas começa com um único passo.");
         Frase.encontrarMaiorFrase("Seja forte e corajoso");
+        Frase.encontrarMaiorFrase("999 999 ");
+        Frase.encontrarMaiorFrase("      ");
 
     }
 }
--- src/main/java/solucaofrase/Frase.java
+++ src/main/java/solucaofrase/Frase.java
@@ -9,11 +9,17 @@ public class Frase {
         for (String p : palavras) {
             // Remove vírgulas, pontos etc.
             p = p.replaceAll("[^a-zA-Zá-úÁ-Ú]", "");
-            if (p.length() > maior.length()) {
-                maior = p;
+            // Valida se contém só letras
+            if (p.matches("[a-zA-Zá-úÁ-Ú]+")) {
+                if (p.length() > maior.length()) {
+                    maior = p;
+                }
             }
         }
-        System.out.println("A maior palavra da frase '"+ frase + "' é: " + maior);
+        if (maior.isEmpty()) {
+            System.out.println("A frase '" + frase + "' contém apenas números ou caracteres inválidos.");
+        } else {
+            System.out.println("A maior palavra da frase '" + frase + "' é: " + maior);
+        }
     }
-
 }
